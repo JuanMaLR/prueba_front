39aea9c1202b6dd6ad5ca7a0bf775c3c932b4254
JuanMaLR <A01206568@itesm.mx>
2022-08-23 09:53:46 -0500

    Formulario basico

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, SafeAreaView, Button, TextInput } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      name: '',
+      email: '',
+      password: ''
+    }
+  }
+
+  submit(){
+    //API call to recieve data
+    console.log("Llegué aquí")
+    fetch('http://10.25.94.215:3000/api/v2/formulario')
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((e) => {
+      console.log(e);
+    });
+  }
+
+  render(){
+    return(
+    <SafeAreaView style={styles.container}>
+      <TextInput placeholder = 'Enter name'
+      onChangeText = {(text) => {this.setState({name: text})}}
+      style = {styles.textInput}/>
+
+      <TextInput placeholder = 'Enter email'
+      onChangeText = {(text) => {this.setState({email: text})}}
+      style = {styles.textInput}/>
+
+      <TextInput placeholder = 'Enter password'
+      secureTextEntry={true}
+      onChangeText = {(text) => {this.setState({password: text})}}
+      style = {styles.textInput}/>
+
+      <Button title = 'Submit' onPress={() => {this.submit()}}/>
+    </SafeAreaView>);
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
+  },
+  textInput: {
+    margin: 20,
+    borderColor: 'black',
+    borderWidth: 2
   },
 });
+
+export default App;
